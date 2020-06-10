9945f727e852c3d6dafdd2544b453715eba4d2b3
Shannon Trainor <sltrainor@outlook.com>
2020-06-10 19:56:35 -0400

    built employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+    constructor(
+        name,
+        role,
+        email,
+        id
+    ) {
+        this.name = name;
+        this.role = role;
+        this.email = email;
+        this.id = id;
+    }
+}
+
+
+//name
+//role
+//email
+//id
